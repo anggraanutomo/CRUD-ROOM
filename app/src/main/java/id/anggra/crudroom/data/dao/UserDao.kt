ecaa723881777fd058f23c6a9e3c1d3a0b9774dd
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2023-01-20 19:22:54 +0700

    project init

---
+++ app/src/main/java/id/anggra/crudroom/data/dao/UserDao.kt
@@ -0,0 +1,23 @@
+package id.anggra.crudroom.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import id.anggra.crudroom.data.entity.User
+
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<User>
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
