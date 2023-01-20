f448fa3d2d0ebeeb7c99087ab9b79a66afc0fe46
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2023-01-20 20:32:50 +0700

    setup MainActivity.kt

--- app/src/main/java/id/anggra/crudroom/MainActivity.kt
+++ app/src/main/java/id/anggra/crudroom/MainActivity.kt
@@ -1,11 +1,52 @@
 package id.anggra.crudroom
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import id.anggra.crudroom.adapter.UserAdapter
+import id.anggra.crudroom.data.AppDatabase
+import id.anggra.crudroom.data.entity.User
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var fab : FloatingActionButton
+    private var list = mutableListOf<User>()
+    private lateinit var adapter: UserAdapter
+    private lateinit var database: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler_view)
+        fab = findViewById(R.id.fab)
+
+        database = AppDatabase.getInstance(applicationContext)
+        adapter = UserAdapter(list)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        recyclerView.addItemDecoration(DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL))
+
+        fab.setOnClickListener{
+            startActivity(Intent(this, EditorActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        list.clear()
+        getData()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun getData() {
+        list.addAll(database.userDao().getAll())
+        adapter.notifyDataSetChanged()
     }
 }
